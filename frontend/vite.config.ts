521ea4189e327e96e66cfa30285a52b4734cf636
yakub <dabaayakub@gmail.com>
2026-04-06 19:28:03 +0530

    vite config file

---
+++ frontend/vite.config.ts
@@ -0,0 +1,20 @@
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    proxy: {
+      '/verify': 'http://localhost:8000',
+      '/verify-text': 'http://localhost:8000',
+      '/history': 'http://localhost:8000',
+      '/health': 'http://localhost:8000',
+    }
+  }
+})
